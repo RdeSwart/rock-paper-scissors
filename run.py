04e44b9ecbd52839227a57f41ebda0919014be58
Ruth De Swart <ruthdeswart@outlook.ie>
2024-07-30 11:20:34 +0000

    feat:Add while loop in case user inputs wrong data on main menu

--- run.py
+++ run.py
@@ -29,22 +29,38 @@ def menu():
     print(f"{CYAN} 1. View Game Rules\n")
     print(f"{YELLOW} 2. Start the game\n")
 
-    choice = int(input("Enter your choice:")(choice))
+    choice = int(input("Enter your choice:\n"))
+
+    while choice > 2:
+        choice = int(input(f"{PURPLE}Please enter 1 or 2:\n"))
 
     if choice == 1:
         get_rules()
     elif choice == 2:
         start_game()
 
+    
+
+    
+
 def get_rules():
     """
     Prints rules of the game to terminal if user input is 1
     from main menu
     """
-    print("Winning Rules of the game are as follows:\n"
-        + "Rock vs Paper --> Paper wins\n"
-        + "Rock vs Scissors --> Rock wins\n"
-        + "Scissors vs Paper --> Scissors Wins\n")
+    print(f"{PURPLE}Winning Rules of the game are as follows:\n"
+        + f"{CYAN}Rock vs Paper --> Paper wins\n"
+        + f"{YELLOW}Rock vs Scissors --> Rock wins\n"
+        + f"{CYAN}Scissors vs Paper --> Scissors Wins\n")
+
+
+def start_game():
+    """
+    Main Game Play against computer. User inputs number corresponding
+    to 3 options, computer randomly chooses an option 
+    and compares to see who wins
+    """
+
 
 
 
@@ -53,4 +69,4 @@ def get_rules():
 
 
 menu()
-get_rules()
+
