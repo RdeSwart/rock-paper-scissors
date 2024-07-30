36e99df24d0fe13b9d310c9519bbfdbc73e5959e
Ruth De Swart <ruthdeswart@outlook.ie>
2024-07-30 09:23:13 +0000

    import random and create banner

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+PURPLE = '\033[0;35m'
+banner = f"""
+  {PURPLE} 
+
+
+  ____            _          ____                             ____       _                        
+ |  _ \ ___   ___| | __     |  _ \ __ _ _ __   ___ _ __      / ___|  ___(_)___ ___  ___  _ __ ___ 
+ | |_) / _ \ / __| |/ /     | |_) / _` | '_ \ / _ \ '__|     \___ \ / __| / __/ __|/ _ \| '__/ __|
+ |  _ < (_) | (__|   <   _  |  __/ (_| | |_) |  __/ |     _   ___) | (__| \__ \__ \ (_) | |  \__ \
+ |_| \_\___/ \___|_|\_\ ( ) |_|   \__,_| .__/ \___|_|    ( ) |____/ \___|_|___/___/\___/|_|  |___/
+                        |/             |_|               |/                                       
+"""
+print(banner)
