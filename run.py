d5db5a189a8fd46bb4d79a1ea9be447fd80d5d86
Ruth De Swart <ruthdeswart@outlook.ie>
2024-08-03 19:36:26 +0000

    feat: Connect Sheets and check they can retrieve data

--- run.py
+++ run.py
@@ -1,4 +1,23 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("RockPaperScissors")
+
 import random
+
+
+scores = SHEET.worksheet("scores")
+data = scores.get_all_values()
+print(data)
 
 # colour codes
 PURPLE = '\033[0;35m'
